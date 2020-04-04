48c49e756480a23b64e3aa6c82aa7a10363cf7e0
Jerimiah Willhite <j.willhite@hotmail.com>
2020-04-03 18:19:50 -0700

    Add initial example and ideas

---
+++ framework.py
@@ -0,0 +1,18 @@
+"Stay informed, but stay sane"
+
+News app uses sentiment analysis to ensure that people don't read too much negative news without reading positive news.
+Uses topic modeling to divide into major categories, and then further into subcategories
+
+No navigation, just browsing
+adjust weights for suggestions between postive/negative based on how much negative news has been tapped
+
+Long-term:
+    Tag news for users, see what sort of help they might need, then provide 3rd party contacts
+    E.g. searching for coronavirus, pop up with CDC notes and nearest hospital contact info
+
+All experience is based on their reactions
+
+When signing up:
+    Pick what they are interested in, then after onboarding, they go to home feed and see what they like, sorted by date
+
+``
---
+++ project_files/functions.py
@@ -0,0 +1,44 @@
+import newspaper
+import random
+
+sites = [
+    'https://arstechnica.com/',
+    'https://www.bbc.com/news/world',
+    'https://www.c-span.org/',
+    'https://www.cnet.com/',
+    'https://www.defensenews.com/',
+    'http://www.digitaljournal.com/',
+    'https://www.afp.com/en/news-hub',
+    'https://www.ft.com/',
+    'https://news.gallup.com/home.aspx',
+    'https://hbr.org/',
+    'https://www.politico.com/',
+    'https://www.reuters.com/',
+    'https://www.pewresearch.org/',
+    'https://www.economist.com/',
+    'https://www.voanews.com/'
+]
+
+def create_site_dict(lst):
+    site_dict = {}
+    for ii in lst:
+        source = newspaper.build(f'{ii}', memoize_articles=False)
+        site_dict[ii] = [article.url for article in source.articles]
+    return site_dict
+
+site_dict = create_site_dict(sites)
+
+#This flattens the list of lists that is site_dict.values()
+feed = [article for sublist in site_dict.values() for article in sublist]
+
+#this takes a long time
+parsed = [newspaper.Article(ii).download() for ii in feed]
+
+#randomfeed = random.sample(feed, len(feed))
+
+#timefeed = feed.sort(key=lambda art:art.article.publish_date)
+
+print(parsed)
+
+
+
